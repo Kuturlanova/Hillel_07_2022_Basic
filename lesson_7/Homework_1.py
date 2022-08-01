cfedbb83956f28e45739009ed814e98d2a85edd1
Maryna <marinakuturlanova@mail.com>
2022-08-01 13:56:40 +0300

    Upload homework for lesson 7

--- lesson_7/Homework_1.py
+++ lesson_7/Homework_1.py
@@ -13,11 +13,29 @@ class Car:
         print(f"Car name: {self.name}\nYear of issue:{self.year}\nManufacturer:{self.manufacturer}\n"
               f"Engine volume:{self.volume}\nColor:{self.color}\nPrice: {self.price}")
 
-    def show_name(self):
-        print(f"Car name:{self.name}")
+    def name(self):
+        print(f'Car name:{self.name}')
+        return self.name
 
-    def show_year(self):
-        print(f"Year of issue:{self.year}")
+    def year(self):
+        print(f'Year of issue:{self.year}')
+        return self.year
+
+    def manufacturer(self):
+        print(f'Manufacturer:{self.manufacturer}')
+        return self.manufacturer
+
+    def volume(self):
+        print(f'Engine volume:{self.volume}')
+        return self.volume
+
+    def color(self):
+        print(f'Color:{self.color}')
+        return self.color
+
+    def price(self):
+        print(f'Price:{self.price}')
+        return self.price
 
 
 honda = Car(name="Honda Accord", year="2019", manufacturer="Honda Motor Co., Ltd.", volume="3,5 l", color="White",
@@ -27,11 +45,23 @@ tesla = Car(name="Tesla Model 3", year="2020", manufacturer="Tesla", volume="Ele
 Car.show_details(honda)
 Car.show_details(tesla)
 
-Car.show_name(honda)
-Car.show_name(tesla)
+Car.name(honda)
+Car.name(tesla)
 
-Car.show_year(honda)
-Car.show_year(tesla)
+Car.year(honda)
+Car.year(tesla)
+
+Car.manufacturer(honda)
+Car.manufacturer(tesla)
+
+Car.volume(honda)
+Car.volume(tesla)
+
+Car.color(honda)
+Car.color(tesla)
+
+Car.price(honda)
+Car.price(tesla)
 
 
 # Homework_2
@@ -55,6 +85,18 @@ class Book:
     def show_edition(self):
         print(f"Publishing house:{self.edition}")
 
+    def show_name(self):
+        print(f"Book name:{self.name}")
+
+    def show_year(self):
+        print(f"Year of publication:{self.year}")
+
+    def show_author(self):
+        print(f"Author:{self.author}")
+
+    def show_price(self):
+        print(f"Price:{self.price}")
+
 
 getsbi = Book(name="The Great Gatsby", year="1925", edition="Scribner’s", genre="Novel",
               author="Francis Scott Key Fitzgerald", price="8 $")
@@ -64,11 +106,23 @@ potter = Book(name="Harry Potter and the Philosopher's Stone", year="1997", edit
 Book.show_details(getsbi)
 Book.show_details(potter)
 
+Book.show_name(getsbi)
+Book.show_name(potter)
+
+Book.show_year(getsbi)
+Book.show_year(potter)
+
 Book.show_genre(getsbi)
 Book.show_genre(potter)
 
 Book.show_edition(getsbi)
 Book.show_edition(potter)
+
+Book.show_author(getsbi)
+Book.show_author(potter)
+
+Book.show_price(getsbi)
+Book.show_price(potter)
 
 
 # Homework_3
@@ -81,16 +135,17 @@ class Stadium:
         self.city = city
         self.placement = placement
 
-
     def show_details(self):
         print(f"Stadium name: {self.name}\nYear of opening:{self.year}\nCountry:{self.country}\n"
               f"City:{self.city}\nPlacement:{self.placement}")
 
     def show_name(self):
         print(f"Stadium name: {self.name}")
+        return self.name
 
     def show_placement(self):
         print(f"Placement:{self.placement}")
+        return self.placement
 
 
 santiago = Stadium(name="Estadio Santiago Bernabéu", year="1947", country="Spain", city="Madrid", placement="81044")
